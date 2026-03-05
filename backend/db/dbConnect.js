3901eadcaf1e583fcf5c969f1e6a363a3723a0a0
Sky28702 <prateekrai462@outlook.com>
2026-03-05 12:59:42 +0530

    full backend setupdone

--- backend/db/dbConnect.js
+++ backend/db/dbConnect.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 async function connectdb() {
   try {
-    const db = await mongoose.connect("url");
+    const db = await mongoose.connect(process.env.MONGO_URL);
+
     console.log("Database connection successfull ! ");
   } catch (error) {
     console.log(error);
--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,14 @@ import express from "express";
 import userRouter from "./routes/route.js";
 import cors from "cors";
 import connectdb from "./db/dbConnect.js";
+import dotenv from "dotenv";
+dotenv.config();
+import { setServers } from "node:dns/promises";
+setServers(["1.1.1.1", "8.8.8.8"]);
 
 const PORT = 8000;
 const app = express();
-// ss
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -16,7 +20,8 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, async () => {
-  console.log(`The server is live at http://localhost:${PORT}`);
+  console.log(`The server is live at http://localhost:${PORT} and connected`);
+  // console.log(process.env.MONGO_URL);
   await connectdb();
 });
 
